db29b10bf07d5861d2b783cb609ea35c8d2db21e
Jackesonleeeeee <43921884+Jackesonleeeeee@users.noreply.github.com>
2019-03-05 12:00:22 -0800

    sea_shore

--- pixlab/PictureExplorer.java
+++ pixlab/PictureExplorer.java
@@ -800,7 +800,7 @@ public class PictureExplorer implements MouseMotionListener, ActionListener, Mou
    */
   public static void main( String args[])
   {
-    Picture pix = new Picture("beach.jpg");
+    Picture pix = new Picture("sea_shore.jpg");
     pix.explore();
   }
   
